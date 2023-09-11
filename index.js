1859057c6d69a7d9d35c9d2a337410647702b8f1
HanggityaRaharjo <hanggitya86@gmail.com>
2023-09-11 20:06:48 +0700

    correct version

--- index.js
+++ index.js
@@ -2,48 +2,35 @@ import plugin from "tailwindcss/plugin";
 import { base } from "./base";
 import { button } from "./button";
 import { color } from "./color";
+import { navbar } from "./navbar";
+import { alert } from "./alert";
+import { badge } from "./badge";
+import { card } from "./card";
+import { progress } from "./progress";
+import { dropdown } from "./dropdown";
+import { table } from "./table";
+import { hero } from "./hero";
+import { footer } from "./footer";
+import { input } from "./input";
+import { select } from "./select";
+import { additional } from "./additional";
+import { toggle } from "./toggle";
 
 export default plugin(({ addBase, addComponents, theme, addUtilities }) => {
-  // Base Root
   addBase(base);
-  // Button Components
-  addComponents(button);
-  // Text Components
-  addComponents(button);
   addUtilities(color, ["responsive", "hover"]);
-  addUtilities({
-    ".modal": {
-      display: "none",
-    },
-    ".modal-box": {
-      display: "flex",
-      flexDirection: "column",
-      alignItems: "center",
-      padding: "2rem",
-      backgroundColor: "red",
-      borderRadius: "0.25rem",
-    },
-    ".modal-action": {
-      display: "flex",
-      justifyContent: "flex-end",
-      marginTop: "1rem",
-    },
-    ".modal[open]": {
-      display: "block",
-    },
-  });
-
-  addComponents({
-    ".dropdown": {
-      position: "relative",
-    },
-    ".dropdown .dropdown-content": {
-      display: "none",
-      position: "absolute",
-      zIndex: "1",
-    },
-    ".dropdown.open .dropdown-content": {
-      display: "block",
-    },
-  });
+  addComponents(additional);
+  addComponents(button);
+  addComponents(navbar);
+  addComponents(alert);
+  addComponents(badge);
+  addComponents(card);
+  addComponents(progress);
+  addComponents(dropdown);
+  addComponents(table);
+  addComponents(hero);
+  addComponents(footer);
+  addComponents(input);
+  addComponents(select);
+  addComponents(toggle);
 });
